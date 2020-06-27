bab92875491a4ad7c8c77c6055d8a2fbc6e162e2
Milena <milena@MacBook-Pro-de-Suporte.local>
2020-06-27 16:10:40 -0300

    remove types

--- src/store/sagas/pokemonTypes.js
+++
@@ -1,9 +0,0 @@
-import { call, put } from 'redux-saga/effects';
-
-import { Creators as PokemonTypeActions } from '../ducks/pokemonTypes';
-
-export function* pokemonTypesRequest(action) {
-  const response = yield call(action.api.get, `/pokemon/${action.count}/`);
-  yield put(PokemonTypeActions.getPokemonTypes(response.data));
-  console.log('response', response.data);
-}
